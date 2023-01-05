9cb1b5a058a868d7cefe74ea1a95d6f2449cedc0
thishastobenoah <thishastobenoah@gmail.com>
2023-01-04 21:19:39 -0500

    changed to david

--- Midterm/src/MidtermProject.java
+++ Midterm/src/MidtermProject.java
@@ -5,7 +5,7 @@ public class MidtermProject {
 		// TODO Auto-generated method stub
 
 		
-		String test = "Once upon a time in the mystical land of Shangrila there dwelt an immortal named Kai-Yun and his trusty band of ninja monkeys.";
+		String test = "Once upon a time in the mystical land of Shangrila there dwelt an immortal named David and his trusty band of ninja monkeys.";
 		String test1 = "Hello Noah was here!";
 	}
 
